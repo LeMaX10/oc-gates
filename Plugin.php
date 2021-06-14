8f69b96d83d52bb1c0af02d96d18aa87dd2d281e
Владимир Пьянков <vladimirpankov@MacBook-Pro-Vladimir.local>
2021-06-14 09:24:56 +0300

    initial

---
+++ Plugin.php
@@ -0,0 +1,62 @@
+<?php
+declare(strict_types=1);
+
+namespace LeMaX10\Gates;
+
+use Auth;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Gate;
+
+/**
+ * Gates Plugin Information File.
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name' => 'lemax10.gates::lang.plugin.name',
+            'description' => 'lemax10.gates::lang.plugin.description',
+            'author' => 'Vladimir Pyankov (aka lemax10)',
+        ];
+    }
+
+    public function register()
+    {
+        $this->registerRequestAndGatesResolvers();
+    }
+
+    public function boot()
+    {
+        $this->registerPolicies();
+    }
+
+    private function registerRequestAndGatesResolvers(): void
+    {
+        $this->app['request']->setUserResolver(function () {
+            return Auth::user();
+        });
+
+        $this->app->singleton(GateContract::class, function ($app) {
+            return new \Illuminate\Auth\Access\Gate(
+                $app,
+                $this->app['request']->getUserResolver()
+            );
+        });
+    }
+
+    /**
+     * Register gate policies.
+     */
+    private function registerPolicies(): void
+    {
+        foreach ($this->policies as $modelNamespace => $policyNamespace) {
+            Gate::policy($modelNamespace, $policyNamespace);
+        }
+    }
+}
